7eca80ba47e85d5d7ead2a9126a269633fa3535f
Azka <a.azkanorouzi@gmail.com>
2021-09-22 12:47:36 +0330

    tablet version has been completed

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 /* selection */
-
+ const toggleBtn=document.querySelector('.toggle');
+ const navLinks=document.querySelector('.nav-links');
+toggleBtnOpen = false;
 /* event handler for toggle btn */
-
+toggleBtn.onclick=function(){
+ navLinks.classList.toggle('nav-links-open')
+ if(toggleBtnOpen === false){
+  /* toggle */
+  toggleBtn.style.transform = "rotate(90deg)"
+  toggleBtnOpen = true;
+ }
+ else{
+  toggleBtn.style.transform = "rotate(0deg)"
+  toggleBtnOpen =false;
+ }
+}
  /* nav links top decoration */
